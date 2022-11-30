b6cb53ff9ff51c77be1d2c7fbdb3d74ad442611d
igrimzyi <1isaiahofficial1@gmail.com>
2022-11-29 20:27:28 -0800

    new

--- backend/config/connect.js
+++ backend/config/connect.js
@@ -7,7 +7,7 @@ let apiPass = process.env.CLUSTER_API_KEY
 //config database and allow export this function to my server
 const connectDB = async () =>{
     try{
-        await mongoose.connect(`mongodb+srv://igrimzyi:Call0fduty@cluster0.zpk5orn.mongodb.net/?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true})
+        await mongoose.connect(`mongodb+srv://igrimzyi:${apiPass}@cluster0.zpk5orn.mongodb.net/?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true})
         console.log('Database connected')
     }catch(err){
         console.error(err.message)
